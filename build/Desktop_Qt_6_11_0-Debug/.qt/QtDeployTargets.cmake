a620c935b392706678b5d04f2b52239ce786a0ac
SabarishCodeWizard <ravik60656@gmail.com>
2026-04-30 13:23:46 +0530

    Clean up tracked build files and apply gitignore

--- build/Desktop_Qt_6_11_0-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_appOCCT_FILE /home/sabarish/occtPro/build/Desktop_Qt_6_11_0-Debug/appOCCT)
-set(__QT_DEPLOY_TARGET_appOCCT_TYPE EXECUTABLE)
--- build/Desktop_Qt_6_11_0-Debug/CMakeFiles/appOCCT_autogen_timestamp_deps.dir/cmake_clean.cmake
+++
@@ -1,5 +0,0 @@
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/appOCCT_autogen_timestamp_deps.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
